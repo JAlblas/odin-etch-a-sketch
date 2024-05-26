fd892a407c766ee6fded51c4e417525b17535553
Jasper Alblas <just_jasper@hotmail.com>
2024-05-26 17:28:52 +0200

    Editing of grid size
    Users can now edit grid size by entering value in input

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 const CONTAINER_WIDTH = 960;
-const SQUARES_PER_SIDE = 16;
 const container = document.querySelector('.container');
+const changeSizeInput = document.querySelector('#changeSizeInput');
+const changeSizeButton = document.querySelector('#changeSizeButton');
+let gridSize = 16;
 
 
-for (let index = 0; index < SQUARES_PER_SIDE; index++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
+changeSizeButton.addEventListener("click", () => {
+    gridSize = changeSizeInput.value;
+    console.log("Changing settings");
 
-    for (let j = 0; j < SQUARES_PER_SIDE; j++) {
+    createGrid();
+})
 
-        let square = document.createElement('div');
-        square.classList.add('square');
+function createGrid() {
+    container.innerHTML = "";
+    for (let index = 0; index < gridSize; index++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
 
-        size = CONTAINER_WIDTH / SQUARES_PER_SIDE;
+        for (let j = 0; j < gridSize; j++) {
 
-        square.style.width = `${size}px`;
-        square.style.height = `${size}px`;
+            let square = document.createElement('div');
+            square.classList.add('square');
 
-        square.addEventListener("mouseenter", (e) => {
-            e.target.style.backgroundColor = "#000";
-        })
-        row.appendChild(square);
+            size = CONTAINER_WIDTH / gridSize;
+
+            square.style.width = `${size}px`;
+            square.style.height = `${size}px`;
+
+            square.addEventListener("mouseenter", (e) => {
+                e.target.style.backgroundColor = "#000";
+            });
+            row.appendChild(square);
+        }
+
+        container.appendChild(row);
+
     }
-
-    container.appendChild(row);
-
 }
+
+createGrid();
