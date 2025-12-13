fbea07f3a51cdcbcb2801262a87e2e89d1ac636c
Jasper Alblas <just_jasper@hotmail.com>
2025-12-13 11:25:48 +0100

    Design updates
    Button and input elements style upgrades

--- script.js
+++ script.js
@@ -1,55 +1,72 @@
-const CONTAINER_WIDTH = 960;
-const container = document.querySelector('.container');
-const changeSizeInput = document.querySelector('#changeSizeInput');
-const changeSizeButton = document.querySelector('#changeSizeButton');
+const CONTAINER_WIDTH = 800;
+let container = document.querySelector("#container");
+let changeSizeInput = document.querySelector("#changeSizeInput");
+let changeSizeButton = document.querySelector("#changeSizeButton");
 let gridSize = 16;
 
-
 changeSizeButton.addEventListener("click", () => {
+  if (changeSizeInput.value > 100) {
+    alert("Max 100 allowed");
+    changeSizeInput.value = 16;
+  } else {
     gridSize = changeSizeInput.value;
-    console.log("Changing settings");
 
     createGrid();
-})
+  }
+});
+
+changeSizeInput.addEventListener("keyup", (e) => {
+  if (e.which == 13) {
+    if (changeSizeInput.value > 100) {
+      alert("Max 100 allowed");
+      changeSizeInput.value = 16;
+    } else {
+      gridSize = changeSizeInput.value;
+
+      createGrid();
+    }
+  }
+});
 
 function createGrid() {
-    container.innerHTML = "";
-    for (let index = 0; index < gridSize; index++) {
-        let row = document.createElement('div');
-        row.classList.add('row');
+  container.innerHTML = "";
+  for (let index = 0; index < gridSize; index++) {
+    let row = document.createElement("div");
+    row.classList.add("column");
 
-        for (let j = 0; j < gridSize; j++) {
+    for (let j = 0; j < gridSize; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
 
-            let square = document.createElement('div');
-            square.classList.add('square');
+      size = CONTAINER_WIDTH / gridSize;
 
-            size = CONTAINER_WIDTH / gridSize;
+      square.style.width = `${size}px`;
+      square.style.height = `${size}px`;
+      square.style.backgroundColor = "#FFF";
+      square.style.opacity = 0;
 
-            square.style.width = `${size}px`;
-            square.style.height = `${size}px`;
-            square.style.backgroundColor = "#FFF";
-            square.style.opacity = 0;
-
-            square.addEventListener("mouseenter", (e) => {
-                if (e.target.style.opacity == 0) {
-                    e.target.style.backgroundColor = random_rgba();
-                    e.target.style.opacity = 0.1;
-                } else {
-                    e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.1;
-                }
-
-            });
-            row.appendChild(square);
+      square.addEventListener("mouseenter", (e) => {
+        if (e.target.style.opacity == 0) {
+          e.target.style.backgroundColor = random_rgba();
+          e.target.style.opacity = 0.1;
+        } else {
+          e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.1;
         }
-
-        container.appendChild(row);
-
+      });
+      row.appendChild(square);
     }
+
+    container.appendChild(row);
+  }
 }
 
 function random_rgba() {
-    var o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + 1 + ')';
+  let o = Math.round,
+    r = Math.random,
+    s = 255;
+  return (
+    "rgba(" + o(r() * s) + "," + o(r() * s) + "," + o(r() * s) + "," + 1 + ")"
+  );
 }
 
 createGrid();
